5a88e44f4bece95bae941db59702f08f6094fae5
Moamin <moamin.salamah@gmail.com>
2022-04-27 10:23:30 +0200

    1st commit

---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='main'>
+        <p className='sign' align='center'>Sign in</p>
+        <form className='form1'>
+            <input className='username' type='text' placeholder='username'/>
+            <input className='password' type='text' placeholder='password'/>
+            <a className='submit' align='center'>Sign in</a>
+            <p className='forgot' align='center'>
+                <a href='#'>Forgot Password? </a>
+            </p>
+        </form>
+    </div>
+  )
+}
+
+export default Form
